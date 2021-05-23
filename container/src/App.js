caf10dfa0a9d26fbd3233971ebf35a37c99f87b2
Hikar1149 <122094854@qq.com>
2021-05-23 23:22:38 +0800

    [+]: common header

--- container/src/App.js
+++ container/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
 export default () => {
   return (
-    <div>
-      <h1>Hi Container!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>Hi Container!</h1>
+        <Header />
+        <hr />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
