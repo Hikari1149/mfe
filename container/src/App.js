f3b5540f9dfd3a92535c2af6c565678b0a3a854b
Hikar1149 <122094854@qq.com>
2021-05-27 22:48:19 +0800

    fix: cls collisions by adding prefix

--- container/src/App.js
+++ container/src/App.js
@@ -2,15 +2,27 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
+
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
+
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <h1>Hi Container!</h1>
-        <Header />
-        <hr />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <h1>Hi Container!</h1>
+          <Header />
+          <hr />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
